341ec7a9205a08ca571f114c10bd3579065e855b
Jessica Lee <jlee1290@terpmail.umd.edu>
2022-09-18 14:40:17 -0400

    Updated is_pacman_in_range method

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -19,7 +19,36 @@ public class Ghost {
     return false;
   }
 
+  /*This method checks its surroundings to see if any Ghosts are in attack 
+  * range. The attack radius of a ghost is 1 which means that a ghost at 
+  * location (x, y) can attack PacMan as long as PacMan is located at 
+  * (x+-1, y+-1). If any Ghosts are in the attack range, this method returns 
+  * true otherwise
+  * it returns false.
+  */
   public boolean is_pacman_in_range() {
+    // all possible attack ranges:
+    Location loc1 = myLoc.shift(-1, -1);
+    Location loc2 = myLoc.shift(-1, 0);
+    Location loc3 = myLoc.shift(-1, 1);
+    Location loc4 = myLoc.shift(0, -1);
+    Location loc5 = myLoc.shift(0, 1);
+    Location loc6 = myLoc.shift(1, -1);
+    Location loc7 = myLoc.shift(1, 0);
+    Location loc8 = myLoc.shift(1, 1);
+
+    if (myMap.getLoc(loc1).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc2).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc3).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc4).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc5).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc6).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc7).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc8).contains(Map.Type.PACMAN)) {
+          
+      return true;  
+    }
+    
     return false;
   }
 
