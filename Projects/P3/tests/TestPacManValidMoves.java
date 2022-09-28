851ab53220b7234236bc3931ec24e34b37330a87
Amal Verma <amalverma99@gmail.com>
2022-09-28 11:48:41 -0400

    Testing PacMan Valid Moves

--- Projects/P3/tests/TestPacManValidMoves.java
+++ Projects/P3/tests/TestPacManValidMoves.java
@@ -1,9 +1,17 @@
 import java.io.*;
+import java.util.ArrayList;
+
 import junit.framework.*;
 
 public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
-    return null;
+    MainFrame frame = new MainFrame();
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+    ArrayList<Location> expected = new ArrayList<>();
+    expected.add(new Location(2, 1));
+    expected.add(new Location(1, 2));
+		assert pacman.get_valid_moves() == expected;
+		frame.startGame();
   }
 }
