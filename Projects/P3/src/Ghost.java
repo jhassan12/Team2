a6ec74772052a4e75ae215385a6700d8b7cbc8c2
jlee882 <87512552+jlee882@users.noreply.github.com>
2022-10-03 10:52:16 -0400

    Ghost is pacman in range (#25)
    * Updated is_pacman_in_range method
    
    * Fix to only up down left right directions
    
    * Update test

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -19,7 +19,28 @@ public class Ghost {
     return false;
   }
 
+  /*This method checks its surroundings to see if any Ghosts are in attack 
+  * range. The attack radius of a ghost is 1 which means that a ghost at 
+  * location (x, y) can attack PacMan as long as PacMan is located at 
+  * (x+-1, y+-1). If any Ghosts are in the attack range, this method returns 
+  * true otherwise
+  * it returns false.
+  */
   public boolean is_pacman_in_range() {
+    // all possible attack ranges:
+    Location loc1 = myLoc.shift(-1, 0);
+    Location loc2 = myLoc.shift(0, -1);
+    Location loc3 = myLoc.shift(0, 1);
+    Location loc4 = myLoc.shift(1, 0);
+
+    if (myMap.getLoc(loc1).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc2).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc3).contains(Map.Type.PACMAN)
+        || myMap.getLoc(loc4).contains(Map.Type.PACMAN)) {
+          
+      return true;  
+    }
+    
     return false;
   }
 
--- Projects/P3/tests/TestPacManInRange.java
+++ Projects/P3/tests/TestPacManInRange.java
@@ -1,9 +1,16 @@
 import java.io.*;
 import junit.framework.*;
+import java.awt.Color;
 
 public class TestPacManInRange extends TestCase {
 
   public void testPacManInRange() throws FileNotFoundException {
-    return null;
+    MainFrame frame = new MainFrame();
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+    Ghost ghost1 = frame.addGhost(new Location(1, 2), "g1", Color.red);
+    Ghost ghost2 = frame.addGhost(new Location(4, 2), "g2", Color.cyan);
+		assert ghost1.is_pacman_in_range() == true;
+		assert ghost2.is_pacman_in_range() == false;
+		frame.startGame();
   }
 }
