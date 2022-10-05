9d403039695806f32f35d4c8847b34ffd2b003cc
dereknop <dereknop@gmail.com>
2022-10-05 12:56:28 -0400

    test for ghost move

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -1,7 +1,14 @@
+import java.awt.Color;
 import java.io.*;
 import junit.framework.*;
 
 public class TestGhostMove extends TestCase {
 
-  public void testGhostMove() throws FileNotFoundException {}
+  public void testGhostMove() throws FileNotFoundException {
+    MainFrame frame = new MainFrame();
+    Ghost testGhost = frame.addGhost(new Location(2, 1), "test ghost", Color.PINK);
+    testGhost.move();
+    assert (testGhost.myLoc.x == 2 && testGhost.myLoc.y == 2);
+    frame.startGame();
+  }
 }
