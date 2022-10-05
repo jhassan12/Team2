5b407d098686367d7f3693da5a3ea5e17cfa5553
dereknop <73036954+dereknop@users.noreply.github.com>
2022-10-05 13:30:23 -0400

    Update TestMapGetLoc.java

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -6,7 +6,7 @@ public class TestMapGetLoc extends TestCase {
   public void testMapGetLoc() throws FileNotFoundException {
     MainFrame frame = new MainFrame();
     frame.addPacMan(new Location(1, 1));
-    assertTrue(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.PACMAN))
+    assertTrue(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.PACMAN));
     frame.startGame();
   }
 }
