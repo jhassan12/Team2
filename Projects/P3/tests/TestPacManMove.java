53f4a2c3a443388f7b3070f27061e88f0ae312d3
dereknop <dereknop@gmail.com>
2022-10-05 13:01:52 -0400

    test for pacman move

--- Projects/P3/tests/TestPacManMove.java
+++ Projects/P3/tests/TestPacManMove.java
@@ -4,6 +4,10 @@ import junit.framework.*;
 public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
-    return null;
+    MainFrame frame = new MainFrame();
+    PacMan pacman = frame.addPacMan(new Location(1, 1));
+    pacman.move();
+    assert (pacman.myLoc.x == 1 && pacman.myLoc.y == 2);
+    frame.startGame();
   }
 }
