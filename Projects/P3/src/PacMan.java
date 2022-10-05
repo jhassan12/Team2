502e6fb9bb1b442fa8a5f8440f22b17c2e561eb9
Jake Perret <44216469+jpperret@users.noreply.github.com>
2022-10-05 11:57:51 -0400

    Ftr pacman (#27)
    * update method
    
    * Use if statement
    
    * test method
    
    * If there is no valid moves, return false. Otherwise make first valid move from get_valid_moves()
    
    * Worked on get_valid_moves() method in PacMan class
    
    * Testing PacMan Valid Moves
    
    * Pacman is ghost in range (#24)
    
    * Updated is_ghost_in_range method
    
    * Fix to only up down left right directions
    
    * Update test
    
    * fixed buy -y to -myLoc.y
    
    Co-authored-by: dereknop <dereknop@gmail.com>
    Co-authored-by: Amal Verma <amalverma99@gmail.com>
    Co-authored-by: jlee882 <87512552+jlee882@users.noreply.github.com>
    Co-authored-by: Amal Verma <75349716+averma99@users.noreply.github.com>
    Co-authored-by: dereknop <73036954+dereknop@users.noreply.github.com>

--- Projects/P3/src/PacMan.java
+++ Projects/P3/src/PacMan.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import javax.swing.JComponent;
 
 public class PacMan {
@@ -14,18 +15,62 @@ public class PacMan {
   }
 
   public ArrayList<Location> get_valid_moves() {
-    return null;
+    ArrayList<Location> moveSet = new ArrayList<>();
+    if (!myMap.getLoc(new Location(myLoc.x, myLoc.y + 1)).contains(Map.Type.WALL) 
+          && !myMap.getLoc(new Location(myLoc.x, myLoc.y + 1)).contains(Map.Type.GHOST)
+          && myLoc.y + 1 < myMap.getDim()) {
+      moveSet.add(new Location(myLoc.x, myLoc.y + 1));
+    }
+    if (!myMap.getLoc(new Location(myLoc.x + 1, myLoc.y)).contains(Map.Type.WALL)
+          && !myMap.getLoc(new Location(myLoc.x, myLoc.y + 1)).contains(Map.Type.GHOST)
+          && myLoc.x + 1 < myMap.getDim()) {
+      moveSet.add(new Location(myLoc.x + 1, myLoc.y));
+    }
+    if (!myMap.getLoc(new Location(myLoc.x - 1, myLoc.y)).contains(Map.Type.WALL)
+          && !myMap.getLoc(new Location(myLoc.x, myLoc.y + 1)).contains(Map.Type.GHOST)
+          && myLoc.x - 1 > 0) {
+      moveSet.add(new Location(myLoc.x - 1, myLoc.y));
+    }
+    if (!myMap.getLoc(new Location(myLoc.x, myLoc.y - 1)).contains(Map.Type.WALL)
+          && !myMap.getLoc(new Location(myLoc.x, myLoc.y + 1)).contains(Map.Type.GHOST)
+          && myLoc.y - 1 > 0) {
+      moveSet.add(new Location(myLoc.x, myLoc.y - 1));
+    }
+
+    return moveSet;
   }
 
   public boolean move() {
+    ArrayList<Location> possibleMoves = get_valid_moves();
+    if (possibleMoves != null) {
+      myLoc.shift(possibleMoves.get(0).x - myLoc.x, possibleMoves.get(0).y - myLoc.y );
+      return true;
+    }
     return false;
   }
 
   public boolean is_ghost_in_range() {
+    // all possible attack ranges:
+    Location loc1 = myLoc.shift(-1, 0);
+    Location loc2 = myLoc.shift(0, -1);
+    Location loc3 = myLoc.shift(0, 1);
+    Location loc4 = myLoc.shift(1, 0);
+
+    if (myMap.getLoc(loc1).contains(Map.Type.GHOST) 
+        || myMap.getLoc(loc2).contains(Map.Type.GHOST)
+        || myMap.getLoc(loc3).contains(Map.Type.GHOST)
+        || myMap.getLoc(loc4).contains(Map.Type.GHOST)) {
+          
+      return true;  
+    }
+
     return false;
   }
 
   public JComponent consume() {
-    return null;
+	  if (myMap.getLoc(myLoc).contains(Map.Type.COOKIE))
+		  return myMap.eatCookie("pacman");
+	  else
+		  return null;
   }
 }
