66e12c26ca68fc0484221683ac78a70751dc93fe
Jake Perret <djakeperret@gmail.com>
2022-10-06 14:02:50 -0400

    Fix Jake Tests

--- Projects/P3/tests/TestConsume.java
+++ Projects/P3/tests/TestConsume.java
@@ -8,8 +8,8 @@ public class TestConsume extends TestCase {
 	public void testConsume() throws FileNotFoundException {
 		MainFrame frame = new MainFrame();
 		PacMan pacman = frame.addPacMan(new Location(1, 1));
-		assert pacman.consume() != null;
-		assert pacman.consume() == null;
+		assertTrue(pacman.consume() != null);
+		assertTrue(pacman.consume() == null);
 		frame.startGame();
 		
 	}
--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -6,8 +6,8 @@ public class TestMapEatCookie extends TestCase {
   public void testMapEatCookie() throws FileNotFoundException {
     MainFrame frame = new MainFrame();
     PacMan pacman = frame.addPacMan(new Location(1, 1));
-    assert frame.getMap().eatCookie("pacman") != null;
-    assert frame.getMap().eatCookie("pacman") == null;
+    assertTrue(frame.getMap().eatCookie("pacman") != null);
+    assertTrue(frame.getMap().eatCookie("pacman") == null);
     frame.startGame();
   }
 }
