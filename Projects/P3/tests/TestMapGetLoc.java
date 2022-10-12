4431c14f6b06ffa03df50d931b73bd6e41b6a2c6
dereknop <dereknop@gmail.com>
2022-10-12 10:01:02 -0400

    fixing tests

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -4,7 +4,7 @@ import junit.framework.*;
 public class TestMapGetLoc extends TestCase {
 
   public void testMapGetLoc() throws FileNotFoundException {
-    MainFrame frame = new MainFrame();
+    NoFrame frame = new NoFrame();
     frame.addPacMan(new Location(1, 1));
     assertTrue(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.PACMAN));
     frame.startGame();
