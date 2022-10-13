34a44af46c3b3714f619aa27456739cdf9dbdc37
Amal Verma <75349716+averma99@users.noreply.github.com>
2022-10-13 17:00:00 -0400

    Actions Amal (#54)
    * Updated main.yaml file
    
    * Debug error with main.yaml file
    
    * Made further changes to main.yaml file
    
    * Made changes to tests
    
    * Made changes to tests

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -1,11 +1,14 @@
+import java.awt.Color;
 import java.io.*;
+
 import junit.framework.*;
 
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
-    MainFrame frame = new MainFrame();
-    assert frame.getMap().move("ghost", new Location(1, 1), Map.Type.GHOST) == true;
+    NoFrame frame = new NoFrame();
+    Ghost ghost = frame.addGhost(new Location(2, 1), "ghost", Color.PINK);
+    assertTrue(frame.getMap().move("ghost", new Location(1, 1), Map.Type.GHOST) == true);
     frame.startGame();
   }
 }
