44c2c535c10c05c270ea993fb31376d12605b907
Amal Verma <amalverma99@gmail.com>
2022-10-13 14:29:04 -0400

    Updated tests

--- Projects/P3/tests/TestPacManValidMoves.java
+++ Projects/P3/tests/TestPacManValidMoves.java
@@ -6,12 +6,14 @@ import junit.framework.*;
 public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
-    MainFrame frame = new MainFrame();
+    NoFrame frame = new NoFrame();
 		PacMan pacman = frame.addPacMan(new Location(1, 1));
     ArrayList<Location> expected = new ArrayList<>();
-    expected.add(new Location(2, 1));
     expected.add(new Location(1, 2));
-		assert pacman.get_valid_moves() == expected;
+    expected.add(new Location(2, 1));
+		assertTrue(pacman.get_valid_moves().size() == expected.size());
+    assertTrue(pacman.get_valid_moves().get(0).equals(expected.get(0)));
+    assertTrue(pacman.get_valid_moves().get(1).equals(expected.get(1)));
 		frame.startGame();
   }
 }
