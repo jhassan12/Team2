fe726b610b6d3a8fcc2676f54595c9eb907be554
dereknop <73036954+dereknop@users.noreply.github.com>
2022-10-14 13:35:23 -0400

    Update TestGhostMove.java

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -8,7 +8,7 @@ public class TestGhostMove extends TestCase {
     NoFrame frame = new NoFrame();
     Ghost testGhost = frame.addGhost(new Location(2, 1), "test ghost", Color.PINK);
     testGhost.move();
-    assert (testGhost.myLoc.x == 2 && testGhost.myLoc.y == 2);
+    assertTrue(testGhost.myLoc.x == 2 && testGhost.myLoc.y == 2);
     frame.startGame();
   }
 }
